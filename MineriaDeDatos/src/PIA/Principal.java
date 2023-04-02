9817906a79854db53a425324b66ebc447cf106e9
Mauricio <mauricio.tamezelperro@gmail.com>
2023-04-02 16:40:18 -0600

    Ajuste de librerías

--- MineriaDeDatos/src/PIA/Principal.java
+++ MineriaDeDatos/src/PIA/Principal.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-
-import javax.naming.InitialContext;
 
 public class Principal {
 	private ArrayList<double[]> dataPoints;
